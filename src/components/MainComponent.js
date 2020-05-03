5e6a0d7526b8eb88dea14fb823201e047ff44859
David Loder <davidloder92@gmail.com>
2020-05-03 12:46:40 -0400

    MainComponent sytax fix

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,7 @@ import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import About from './AboutComponent'
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
