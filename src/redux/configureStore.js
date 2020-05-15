a6443fd182a0a1c981b86860a4f8e6c6fc77bdee
David Loder <davidloder92@gmail.com>
2020-05-14 21:07:18 -0400

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Campsites } from './campsites.js';
 import { Comments } from './comments.js';
 import { Partners } from './partners.js';
 import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +13,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             partners: Partners,
             promotions: Promotions
-        })
+        }),
+        applyMiddleware(thunk, logger)
         
     );
     return store;
